05d73b64b4e1fb0e63453e5ef5e068b21a5becd4
anderwya000 <anderwya000@students.elkhorn.k12.wi.us>
2024-10-24 19:20:39 -0500

    Add pringle coloring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """https://github.com/anderwya000/clicker-game-for-ap-csp"""
 # --------------- Imports ---------------
 from tkinter import *
+from random import randrange
 # --------------- Functions ---------------
 
 
@@ -21,11 +22,24 @@ def calc_numbers():
 
 def click(event):
     """Click event handler. Gives more pringles."""
+    global color_index
     money.set(money.get() + strength.get())
+    pringle.itemconfig(normal_pringle, state='normal')
+    pringle.itemconfig(green_pringle, state='hidden')
+    pringle.itemconfig(blue_pringle, state='hidden')
+    pringle.itemconfig(red_pringle, state='hidden')
+    if randrange(0,1001) == 1000:
+        color_index += 1
+        if color_index > 2:
+            color_index = 0
+        pringle.itemconfig(normal_pringle, state='hidden')
+        pringle.itemconfig(pringle_colors[color_index], state='normal')
+        money.set(money.get() + (1000 * strength.get()))
+
 
 
 def auto_click_upgrade():
-    """Check if the autoclicker upgrade is avaliable, then buy it."""
+    """Check if the autoclicker upgrade is available, then buy it."""
     if money.get() >= clicker_cost.get():
         price = clicker_cost.get()
         money.set(money.get() - price)
@@ -35,7 +49,7 @@ def auto_click_upgrade():
 
 
 def strength_upgrade():
-    """Check if the strength upgrade is avaliable, then buy it."""
+    """Check if the strength upgrade is available, then buy it."""
     if money.get() >= strength_cost.get():
         price = strength_cost.get()
         money.set(money.get() - price)
@@ -62,7 +76,7 @@ strength.set(1)
 clickers = IntVar()
 clickers.set(0)
 pringles_count = StringVar()
-
+color_index = 0
 
 root.geometry('880x460')
 root.configure(background='#F5F5DC')
@@ -72,10 +86,31 @@ run_clicks()
 calc_numbers()
 
 pringle = Canvas(root, height=256, width=256, bg='#F5F5DC', highlightthickness=0)
-picture_file = PhotoImage(file='pringle.gif')
-picture_file = picture_file.zoom(3, 3)
-picture_file = picture_file.subsample(7, 7)
-pringle.create_image(128, 128, image=picture_file)
+picture_file1 = PhotoImage(file='normal_pringle.gif')
+picture_file1 = picture_file1.zoom(3)
+picture_file1 = picture_file1.subsample(7)
+normal_pringle = pringle.create_image(128, 128, image=picture_file1)
+pringle.itemconfig(normal_pringle, state='normal')
+
+picture_file2 = PhotoImage(file='green_pringle.gif')
+picture_file2 = picture_file2.zoom(3)
+picture_file2 = picture_file2.subsample(7)
+green_pringle = pringle.create_image(128, 128, image=picture_file2)
+pringle.itemconfig(green_pringle, state='hidden')
+
+picture_file3 = PhotoImage(file='blue_pringle.gif')
+picture_file3 = picture_file3.zoom(3)
+picture_file3 = picture_file3.subsample(7)
+blue_pringle = pringle.create_image(128, 128, image=picture_file3)
+pringle.itemconfig(blue_pringle, state='hidden')
+
+picture_file4 = PhotoImage(file='red_pringle.gif')
+picture_file4 = picture_file4.zoom(3)
+picture_file4 = picture_file4.subsample(7)
+red_pringle = pringle.create_image(128, 128, image=picture_file4)
+pringle.itemconfig(red_pringle, state='hidden')
+
+pringle_colors = [green_pringle, blue_pringle, red_pringle]
 pringle.place(x=50, y=75)
 pringle.bind("<Button-1>", click)
 
@@ -86,9 +121,6 @@ Label(root, textvariable=pringles_count, bg='#F5F5DC', font=('helvetica', 24, 'b
 
 # LABEL: ↑ Click it! ↑
 Label(root, text='↑ Click it! ↑', bg='#F5F5DC', font=('helvetica', 24, 'bold')).place(x=90, y=360)
-
-# LABELFRAME: upgrades area
-
 
 # LABEL: Upgrades
 Label(root, text='Upgrades', bg='#F5F5DC', font=('helvetica', 22, 'bold')).place(x=415, y=60)
