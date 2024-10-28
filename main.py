86591549c02835e90a248df4959463dfd5327147
LukeSaverCool <yoldog5678@gmail.com>
2024-10-28 07:51:39 -0500

    Add code credit to main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 """https://github.com/anderwya000/clicker-game-for-ap-csp"""
 # --------------- Imports ---------------
+# --------------- Luke ---------------
 from tkinter import *
 from random import randrange
 # --------------- Functions ---------------
 
-
+# --------------- Wyatt ---------------
 def run_clicks():
     """Calculate the autoclicker gain."""
     money.set(money.get() + (strength.get() * clickers.get()))
     root.after(1000, run_clicks)
 
-
+# --------------- Wyatt ---------------
 def calc_numbers():
     """Calculate the number to display as pringles."""
     if money.get() > 999999:
@@ -19,7 +20,7 @@ def calc_numbers():
         pringles_count.set(f'{money.get()} pringles')
     root.after(10, calc_numbers)
 
-
+# --------------- Wyatt ---------------
 def click(event):
     """Click event handler. Gives more pringles."""
     global color_index
@@ -35,8 +36,7 @@ def click(event):
         pringle.itemconfig(pringle_colors[color_index], state='normal')
         # money.set(money.get() + (1000 * strength.get()))
 
-
-
+# --------------- Wyatt ---------------
 def auto_click_upgrade():
     """Check if the autoclicker upgrade is available, then buy it."""
     if money.get() >= clicker_cost.get():
@@ -46,7 +46,7 @@ def auto_click_upgrade():
         clicker_cost.set(new_price)
         clickers.set(clickers.get() + 1)
 
-
+# --------------- Luke ---------------
 def strength_upgrade():
     """Check if the strength upgrade is available, then buy it."""
     if money.get() >= strength_cost.get():
@@ -56,14 +56,14 @@ def strength_upgrade():
         strength_cost.set(new_price)
         strength.set(strength.get() + 1)
 
-
+# --------------- Luke ---------------
 def convert_to_e(number):
     """Convert a number to scientific notation."""
     return '{:.2e}'.format(number)
 
 
 root = Tk()
-
+# --------------- Wyatt ---------------
 money = IntVar()
 money.set(0)
 clicker_cost = IntVar()
@@ -76,21 +76,21 @@ clickers = IntVar()
 clickers.set(0)
 pringles_count = StringVar()
 color_index = 0
-
+# --------------- Luke ---------------
 root.geometry('880x460')
 root.configure(background='#F5F5DC')
 root.title('Pringle Clicker')
 
 run_clicks()
 calc_numbers()
-
+# --------------- Luke ---------------
 pringle = Canvas(root, height=256, width=256, bg='#F5F5DC', highlightthickness=0)
 picture_file1 = PhotoImage(file='normal_pringle.gif')
 picture_file1 = picture_file1.zoom(3)
 picture_file1 = picture_file1.subsample(7)
 normal_pringle = pringle.create_image(128, 128, image=picture_file1)
 pringle.itemconfig(normal_pringle, state='normal')
-
+# --------------- Wyatt ---------------
 picture_file2 = PhotoImage(file='green_pringle.gif')
 picture_file2 = picture_file2.zoom(3)
 picture_file2 = picture_file2.subsample(7)
@@ -108,19 +108,19 @@ picture_file4 = picture_file4.zoom(3)
 picture_file4 = picture_file4.subsample(7)
 red_pringle = pringle.create_image(128, 128, image=picture_file4)
 pringle.itemconfig(red_pringle, state='hidden')
-
+# --------------- Wyatt ---------------
 pringle_colors = [normal_pringle, green_pringle, blue_pringle, red_pringle]
 pringle.place(x=50, y=75)
 pringle.bind("<Button-1>", click)
 
 # --------------- User Interface ---------------
-
+# --------------- Luke ---------------
 # LABEL: Money (pringles_count)
 Label(root, textvariable=pringles_count, bg='#F5F5DC', font=('helvetica', 24, 'bold')).place(x=100, y=20)
 
 # LABEL: ↑ Click it! ↑
 Label(root, text='↑ Click it! ↑', bg='#F5F5DC', font=('helvetica', 24, 'bold')).place(x=90, y=360)
-
+# --------------- Wyatt ---------------
 # FRAME: Upgrades
 upgrade_frame = Frame(root, bg='#CF9E54', bd=5, relief=SUNKEN)
 upgrade_frame.place(x=350, y=110)
@@ -130,7 +130,7 @@ upgrade_frame.columnconfigure(2, weight=1)
 upgrade_frame.rowconfigure(0, weight=1)
 upgrade_frame.rowconfigure(1, weight=1)
 upgrade_frame.rowconfigure(2, weight=1)
-
+# --------------- Luke ---------------
 # LABEL: Upgrades
 Label(upgrade_frame, text='Upgrades', bg='#CF9E54', font=('helvetica', 22, 'bold')).grid(column=0, row=0, padx=10, pady=10)
 
@@ -142,13 +142,13 @@ Button(upgrade_frame, text='Click strength', bg='#CDB79E', font=('helvetica', 20
 
 # LABEL: Cost
 Label(upgrade_frame, text='Cost', bg='#CF9E54', font=('helvetica', 22, 'bold')).grid(column=1, row=0, padx=10, pady=10)
-
+# --------------- Wyatt, Luke ---------------
 # LABEL: Autoclicker Cost (clicker_cost)
 Label(upgrade_frame, textvariable=clicker_cost, bg='#CF9E54', font=('helvetica', 20, 'bold')).grid(column=1, row=1, padx=10, pady=10)
 
 # LABEL: Strength Cost (strength_cost)
 Label(upgrade_frame, textvariable=strength_cost, bg='#CF9E54', font=('helvetica', 20, 'bold')).grid(column=1, row=2, padx=10, pady=10)
-
+# --------------- Luke ---------------
 # LABEL: Amount Owned
 Label(upgrade_frame, text='Amount', bg='#CF9E54', font=('helvetica', 22, 'bold')).grid(column=2, row=0, padx=10, pady=10)
 
